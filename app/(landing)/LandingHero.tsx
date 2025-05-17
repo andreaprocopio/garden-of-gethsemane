aae978ef2d422035abef64a28c5773c703981730
andreaprocopio <andreproco@gmail.com>
2025-05-17 18:07:49 +0200

    inserting bg in hero text

--- app/(landing)/LandingHero.tsx
+++ app/(landing)/LandingHero.tsx
@@ -11,7 +11,7 @@ export function LandingHero() {
         transition={{ duration: 0.5, ease: [0.4, 0.0, 0.2, 1] }}
         className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
       >
-        Find your calm, focus your mind, and enhance your well-being with{" "}
+        Find your calm, focus your mind, and enhance your well-being with <br />
         <Highlight className="text-black dark:text-white">
           immersive soundscapes.
         </Highlight>
